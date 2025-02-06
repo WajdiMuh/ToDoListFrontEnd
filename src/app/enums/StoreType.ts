85acd3058e793e58995a96e75e453fd537218cbf
WajdiMuh <w.elmuhtadi@gju.edu.jo>
2025-02-06 23:40:45 +0100

    update path to assets

--- src/app/enums/StoreType.ts
+++ src/app/enums/StoreType.ts
@@ -5,9 +5,9 @@ export enum StoreType {
 }
 
 let StoreTypeIcon: Map<StoreType, string> = new Map<StoreType, string>([
-    [StoreType.Grocery, "../assets/grocery.svg"],
-    [StoreType.Cosmetics, "../assets/cosmetics.svg"],
-    [StoreType.Miscellaneous, "../assets/miscellaneous.svg"],
+    [StoreType.Grocery, "assets/grocery.svg"],
+    [StoreType.Cosmetics, "assets/cosmetics.svg"],
+    [StoreType.Miscellaneous, "assets/miscellaneous.svg"],
 ]);
 
 export {StoreTypeIcon};
